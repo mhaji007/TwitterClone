ea823d6771abc31ca8384b0ad98c284b7a165831
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-03-10 23:39:12 -0500

    Set up styles for login

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 require("dotenv").config();
-
 
 const app = express();
 
 const port = process.env.PORT;
-const {requireLogin} = require("./middleware")
+const { requireLogin } = require("./middleware");
 
-const server = app.listen(port, () => console.log("Server listening on port " + port))
+const server = app.listen(port, () =>
+  console.log("Server listening on port " + port)
+);
 
 // Tell server which template engine to use
 app.set("view engine", "pug");
 // Tell server where to find pug template files
-app.set("views", "views")
+app.set("views", "views");
 
-app.use(express.static(path.join(__dirname,"public")))
+// Static fies are files that are not processed by the server
+// before being handed down to the client (browser)
+// __dirname gives absolute path to the file that is currently running
+app.use(express.static(path.join(__dirname, "public")));
 
-const loginRoute = require("./routes/loginRoutes")
+const loginRoute = require("./routes/loginRoutes");
 
-app.use("/login", loginRoute)
+// Route middlewares
+app.use("/login", loginRoute);
 
 app.get("/", requireLogin, (req, res, next) => {
   // Dynamic page title
   // Dynamically pass data from server to template
   // payload sen to to and accessed in home.pug
   var payload = {
-    pageTitle:"Home"
-  }
-  res.status(200).render("home", payload)
-})
+    pageTitle: "Home",
+  };
+  res.status(200).render("home", payload);
+});
