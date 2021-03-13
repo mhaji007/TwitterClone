6ed3301c5099828ea4acdcc326aea91950e699d2
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-03-13 14:05:11 -0500

    Retrieve entered text from textbox

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,10 @@
 // Houses all the shared code - will be included on all the pages
 
 // Wait until all dependencies have been loaded
-$(document).ready(() => {alert('Testing')})
+// $(document).ready(() => {alert('Testing')})
+
+
+$("#postTextarea").keyup((event)=>{
+  var textbox = $(event.target)
+  var value = textbox.val().trim()
+})
