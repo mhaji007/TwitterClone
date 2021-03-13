b6401341e93e24abb6d575523aebc220e61aa114
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-03-12 22:04:26 -0500

    Set default photo

--- models/user.js
+++ models/user.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
   },
   profilePicture: {
     type: String,
-    default: "/images/profilePic.png",
+    default: "/images/profile.jpg",
   },
 }, {timestamps: true});
 
