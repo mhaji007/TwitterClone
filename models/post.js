2f26a72807baabc487f945f45d0f50ee74d89d37
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-03-15 21:46:13 -0400

    Add like field to user and post schema

--- models/post.js
+++ models/post.js
@@ -1,16 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
+const postSchema = new mongoose.Schema(
+  {
+    // required:true is not set here
+    // since when re-tweeting there is no content
+    content: { type: String, trim: true },
 
-const postSchema = new mongoose.Schema({
-  // required:true is not set here
-  // since when re-tweeting there is no content
-  content:{type:String, trim:true},
-
-  postedBy: {type:ObjectId, ref: "User"},
-  pinned: Boolean
-
-}, {timestamps:true})
-
+    postedBy: { type: ObjectId, ref: "User" },
+    // Array of all users that have liked a given post
+    likes: [{ type: ObjectId, ref: "User" }],
+    pinned: Boolean,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Post", postSchema);
--- models/user.js
+++ models/user.js
@@ -31,6 +31,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "/images/profile.jpg",
   },
+
+  // Array of posts user has liked
+  // If we did not keep track of likes her
+  // It would mean to get all the posts that a user liked
+  // we need to query every single post to check them all to
+  // see which one contained the user which
+  // is quite an expensive operation
+
+  likes:[{type: ObjectId, ref:"Post"}]
 }, {timestamps: true});
 
 module.exports = mongoose.model("User", userSchema);
