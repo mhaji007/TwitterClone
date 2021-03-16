358cd1bea003ab59cb12a57f6d3c87f0558bd469
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-03-15 21:30:36 -0400

    Calculate user-friendly timestamps for posts

--- public/js/common.js
+++ public/js/common.js
@@ -28,8 +28,8 @@ $("#submitPostButton").click((event) => {
 
   $.post("/api/posts", data, (postData, status, xhr) => {
     var html = createPostHtml(postData);
-    // Add to the start of the postContainer
-    $(".postContainer").prepend(html);
+    // Add to the start of the postsContainer
+    $(".postsContainer").prepend(html);
     textbox.val("");
     // When JQuery is used to remove the text
     // key up event is not triggered and code
@@ -41,13 +41,15 @@ $("#submitPostButton").click((event) => {
 
 function createPostHtml(postData) {
    var postedBy = postData.postedBy;
-
+  // Check to see whether postedBy is populated on the server
    if(postedBy._id === undefined) {
+    //  postedBy is just an objectId
      return console.log("User object not populated")
    }
 
    var displayName = postedBy.firstName + " " + postedBy.lastName;
-   var timestamp = postData.createdAt;
+  //  var timestamp = postData.createdAt;
+   var timestamp = timeDifference(new Date(), new Date(postData.createdAt));
 
   return `<div class='post'>
 
@@ -84,4 +86,30 @@ function createPostHtml(postData) {
                   </div>
               </div>
           </div>`;
+}
+
+function timeDifference(current, previous) {
+  var msPerMinute = 60 * 1000;
+  var msPerHour = msPerMinute * 60;
+  var msPerDay = msPerHour * 24;
+  var msPerMonth = msPerDay * 30;
+  var msPerYear = msPerDay * 365;
+
+  var elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    if (elapsed / 1000 < 30) return "Just now";
+
+    return Math.round(elapsed / 1000) + " seconds ago";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + " hours ago";
+  } else if (elapsed < msPerMonth) {
+    return Math.round(elapsed / msPerDay) + " days ago";
+  } else if (elapsed < msPerYear) {
+    return Math.round(elapsed / msPerMonth) + " months ago";
+  } else {
+    return Math.round(elapsed / msPerYear) + " years ago";
+  }
 }
